49f5d413b458ec522da1d2a5ad1e41003c081948
Rasal Hossain Adib <adibrasel.2022@gmail.com>
2023-08-11 03:43:52 +0600

    File folder setup
    File folder setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "./App.css"
+
+const App = () => {
+  return (
+    <h1 className='underline text-3xl text-red-500'>App</h1>
+  )
+}
+
+export default App
+
+
+// Class Link = https://www.youtube.com/watch?v=jx5hdo50a2M&t=481s
